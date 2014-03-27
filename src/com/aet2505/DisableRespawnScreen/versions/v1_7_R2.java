4eb17eef66aedee3a8ebe7163398d02d9079c32f
yoshi435 <kousuke_an_idiot10@yahoo.co.jp>
2014-03-27 14:41:10 +0900

    Create v1_7_R2.java
    Working on Bukkit 1.7.5 R0.1

---
+++ src/com/aet2505/DisableRespawnScreen/versions/v1_7_R2.java
@@ -0,0 +1,40 @@
+package com.aet2505.DisableRespawnScreen.versions;
+
+import net.minecraft.server.v1_7_R2.EnumClientCommand;
+import net.minecraft.server.v1_7_R2.PacketPlayInClientCommand;
+
+import org.bukkit.craftbukkit.v1_7_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import com.aet2505.DisableRespawnScreen.Main;
+import com.aet2505.DisableRespawnScreen.NMS;
+
+public class v1_7_R2 implements NMS, Listener {
+	private Main plugin;
+
+	@Override
+	public void registerDeathListener(Main plugin) {
+		this.plugin = plugin;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		plugin.logger.info("Death Listener Registered");
+
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		final Player player = e.getEntity();
+		plugin.getServer().getScheduler()
+				.scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						if (player.isDead()) {
+							((CraftPlayer) player).getHandle().playerConnection
+									.a(new PacketPlayInClientCommand(
+											EnumClientCommand.PERFORM_RESPAWN));
+						}
+					}
+				});
+	}
+}
